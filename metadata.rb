46662772c7c55bcfd32dc4839b46656e90c2e5d9
Matt Stratton <matt.stratton@gmail.com>
2015-05-18 21:45:40 -0500

    inital commit

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'webserver'
+maintainer       'Matt Stratton'
+maintainer_email 'mattstratton@chef.io'
+license          'apache2'
+description      'Installs/Configures webserver'
+long_description 'Installs/Configures webserver'
+version          '0.1.0'
+
